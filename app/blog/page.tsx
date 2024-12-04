215e51b2d3d06de636770430ac41164c6d6042b1
akiJacky <tyatya38an@gmail.com>
2024-12-05 04:10:27 +0900

    add pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,43 @@
+// app/blog/page.tsx
+"use client"; // クライアントサイドコンポーネントとしてマーク
+
+import { useEffect, useState } from "react";
 import BlogRows from "@/components/routes/BlogRows";
+import Pagination from "@/components/routes/Pagination";
 
 const BlogPage = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [totalBlogs, setTotalBlogs] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // APIからブログデータを取得
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await fetch(`/api/blog?page=${currentPage}`);
+      const data = await response.json();
+      setBlogs(data.blogs);
+      setTotalBlogs(data.total);
+    };
+
+    fetchBlogs();
+  }, [currentPage]);
+
+  // ページネーションのページ切り替え
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="m-8">
       <h1 className="mb-2">Blog</h1>
-      <BlogRows />
+      <BlogRows blogs={blogs} />
+      <div className="my-8">
+        <Pagination
+          currentPage={currentPage}
+          totalBlogs={totalBlogs}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 };
--- components/routes/BlogRows.tsx
+++ components/routes/BlogRows.tsx
@@ -1,64 +1,52 @@
-"use client";
-
-import { useEffect, useState } from "react";
+// components/routes/BlogRows.tsx
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 
-// Blogの型定義
 interface Blog {
   number: string;
   title: string;
   lastUpdate: string;
 }
 
-const fetchBlogs = async () => {
-  const response = await fetch("/api/blog");
-  const data = await response.json();
-  return data;
-};
+interface BlogRowsProps {
+  blogs: Blog[];
+}
 
-const BlogPage = () => {
-  // useStateに型を指定
-  const [blogs, setBlogs] = useState<Blog[]>([]);
-
-  useEffect(() => {
-    const getBlogs = async () => {
-      const blogData = await fetchBlogs();
-      setBlogs(blogData);
-    };
-    getBlogs();
-  }, []);
-
+const BlogRows = ({ blogs }: BlogRowsProps) => {
   return (
-    <div>
-      <Table>
-        <TableCaption>Blogs</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">No.</TableHead>
-            <TableHead>Title</TableHead>
-            <TableHead className="text-right">Update time</TableHead>
+    <Table>
+      <TableCaption>Blogs</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">No.</TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead className="text-right">Update time</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {blogs.map((blog) => (
+          <TableRow key={blog.number}>
+            <TableCell className="font-medium">{blog.number}</TableCell>
+            <TableCell>{blog.title}</TableCell>
+            <TableCell className="text-right">{blog.lastUpdate}</TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {blogs.map((blog) => (
-            <TableRow key={blog.number}>
-              <TableCell className="font-medium">{blog.number}</TableCell>
-              <TableCell>{blog.title}</TableCell>
-              <TableCell className="text-right">{blog.lastUpdate}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
   );
 };
 
-export default BlogPage;
+export default BlogRows;
