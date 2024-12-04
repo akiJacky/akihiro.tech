c2bf51c44f9ca2ac18c15f4a2a47d79038f3f01a
akiJacky <tyatya38an@gmail.com>
2024-12-05 03:45:30 +0900

    add api

--- components/routes/BlogRows.tsx
+++ components/routes/BlogRows.tsx
@@ -1,78 +1,64 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 
-const blogs = [
-  {
-    number: "001",
-    title: "Exploring New Digital Horizons",
-    lastUpdate: "2024-11-10",
-  },
-  {
-    number: "002",
-    title: "Innovative Solutions for Tomorrow",
-    lastUpdate: "2024-11-10",
-  },
-  {
-    number: "003",
-    title: "Creating Success Through Design",
-    lastUpdate: "2024-11-10",
-  },
-  {
-    number: "004",
-    title: "Empowering Growth with Technology",
-    lastUpdate: "2024-11-10",
-  },
-  {
-    number: "005",
-    title: "Transforming Ideas Into Reality",
-    lastUpdate: "2024-11-10",
-  },
-  {
-    number: "006",
-    title: "Unlocking Potential Through Creativity",
-    lastUpdate: "2024-11-10",
-  },
-  {
-    number: "007",
-    title: "Shaping the Future with Innovation",
-    lastUpdate: "2024-11-10",
-  },
-];
-
-export default function BlogRows() {
-  return (
-    <Table>
-      <TableCaption>Blogs</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">No.</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead className="text-right">Update time</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {blogs.map((blog) => (
-          <TableRow key={blog.number}>
-            <TableCell className="font-medium">{blog.number}</TableCell>
-            <TableCell>{blog.title}</TableCell>
-            <TableCell className="text-right">{blog.lastUpdate}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right"></TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
-  );
+// Blogの型定義
+interface Blog {
+  number: string;
+  title: string;
+  lastUpdate: string;
 }
+
+const fetchBlogs = async () => {
+  const response = await fetch("/api/blog");
+  const data = await response.json();
+  return data;
+};
+
+const BlogPage = () => {
+  // useStateに型を指定
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    const getBlogs = async () => {
+      const blogData = await fetchBlogs();
+      setBlogs(blogData);
+    };
+    getBlogs();
+  }, []);
+
+  return (
+    <div>
+      <Table>
+        <TableCaption>Blogs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">No.</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead className="text-right">Update time</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {blogs.map((blog) => (
+            <TableRow key={blog.number}>
+              <TableCell className="font-medium">{blog.number}</TableCell>
+              <TableCell>{blog.title}</TableCell>
+              <TableCell className="text-right">{blog.lastUpdate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default BlogPage;
